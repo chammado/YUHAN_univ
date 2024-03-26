71e49003364e36d754e768259de1b3af4093af6d
unknown <kimbklove@gmail.com>
2024-03-26 23:17:08 +0900

    3주차 과제 제출3주차 과제 제출3주차 과제 제출

---
+++ FirstProject/FirstProject/3thClass.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	int num;
+	std::string name;
+	std::cout << "백호영: 아힘들다...." << std:: endl;
+	std::cout << "나쁜놈의 이름과 나이를 입력하세요: ";
+	std::cin >> name >> num;
+	std::cout << "백호영: "<< name << "이..." << num << std::endl;
+
+	while (num)
+	{
+
+	}
+
+	return 0;
+}
---
+++ FirstProject/FirstProject/Test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main02()
+{
+	std::cout << "±èº´±Ô: ¾ÆÈûµé´Ù...." << std::endl;
+
+	return 0;
+}
